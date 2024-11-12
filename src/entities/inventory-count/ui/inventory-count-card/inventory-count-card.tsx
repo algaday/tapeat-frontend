712cd50d60f5c48357bebcb499723216e2191733
Ansar <annssarr@gmail.com>
2024-11-12 22:16:47 +0500

    feat: inventory count card

---
+++ src/entities/inventory-count/ui/inventory-count-card/inventory-count-card.tsx
@@ -0,0 +1,27 @@
+import { Box } from '@mui/material';
+
+import { InventoryCount } from '@entities/inventory-count/api/types';
+import { formatDate } from '@shared/utils/formatNumber';
+
+import { BoldTypography, CaptionTypography, CardLayout, InventoryCountHeader } from './style';
+
+interface Props {
+  completionStatus: React.ReactNode;
+  inventoryCount: InventoryCount;
+}
+
+export function InventoryCountCard({
+  completionStatus,
+  inventoryCount: { branchName, createdAt, templateName },
+}: Props) {
+  return (
+    <CardLayout component={'article'}>
+      <InventoryCountHeader>
+        <BoldTypography>{formatDate(createdAt)}</BoldTypography>
+        <Box>{completionStatus}</Box>
+      </InventoryCountHeader>
+      <BoldTypography>{templateName}</BoldTypography>
+      <CaptionTypography>{branchName}</CaptionTypography>
+    </CardLayout>
+  );
+}
