95f44d4f32f41d21fbcf4920a3c4fc3ffba255e2
Ansar <annssarr@gmail.com>
2024-11-21 23:11:32 +0500

    fix: .env bug

--- src/shared/api/base-query.ts
+++ src/shared/api/base-query.ts
@@ -1,5 +1,7 @@
+'use client';
 import { fetchBaseQuery } from '@reduxjs/toolkit/query';
 
+console.log(process.env.NEXT_PUBLIC_BASE_API);
 export const baseQuery = fetchBaseQuery({
   baseUrl: process.env.NEXT_PUBLIC_BASE_API,
   credentials: 'include',
