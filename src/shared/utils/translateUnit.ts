f13c7a6638913756a4f65176170db41a4f08993d
Ansar <annssarr@gmail.com>
2024-11-23 18:06:21 +0500

    refactor: move translateUnit to shared/utils

---
+++ src/shared/utils/translateUnit.ts
@@ -0,0 +1,18 @@
+import { IngredientUnit } from '@shared/constants';
+
+export const translateUnit = (unit: IngredientUnit): string => {
+  switch (unit) {
+    case 'litre':
+      return 'литр';
+    case 'ml':
+      return 'мл';
+    case 'kg':
+      return 'кг';
+    case 'g':
+      return 'грамм';
+    case 'piece':
+      return 'штук';
+    default:
+      return unit;
+  }
+};
