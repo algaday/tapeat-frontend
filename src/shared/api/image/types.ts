df724feb7d5302cbd0cf110973d64a6c1635c159
Kainar Massujima <masujimk@gmail.com>
2025-03-25 21:40:11 +0500

    fix: fix build

--- src/shared/api/image/types.ts
+++ src/shared/api/image/types.ts
@@ -1,17 +1,20 @@
+'use client';
+
 import { z } from 'zod';
 
 const MAX_UPLOAD_SIZE = 1024 * 1024 * 3; // 3MB
 const ACCEPTED_FILE_TYPES = ['image/png'];
 
-export const createImageDto = z
-  .instanceof(File)
-  .refine((file) => {
-    return !file || file.size <= MAX_UPLOAD_SIZE;
-  }, 'File size must be less than 3MB')
-  .refine((file) => {
-    return ACCEPTED_FILE_TYPES.includes(file!.type);
-  }, 'File must be a PNG');
+let createImageDto: z.ZodType<File>;
 
+if (typeof window !== 'undefined') {
+  createImageDto = z
+    .instanceof(File)
+    .refine((file) => file.size <= MAX_UPLOAD_SIZE, 'File size must be less than 3MB')
+    .refine((file) => ACCEPTED_FILE_TYPES.includes(file.type), 'File must be a PNG');
+} else {
+  createImageDto = z.any(); // или вообще не использовать на сервере
+}
 export const ImageSchema = z.object({
   id: z.string(),
   restaurantId: z.string(),
